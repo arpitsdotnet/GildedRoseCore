c7faf42a2b71b2b59cf734cfb4df5b4ab770c021
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-09 15:57:22 +0530

    Convert all else into if, revert if if neccessary

--- src/GildedRoseCore.Console/GildedRose.cs
+++ src/GildedRoseCore.Console/GildedRose.cs
@@ -20,46 +20,16 @@ namespace GildedRoseCore.Console
 
             foreach (var item in _items)
             {
-                if (item.Name != Constants.AGED_BRIE && item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS)
-                            item.Quality = item.Quality - 1;
-                    }
-                }
-                else if (item.Quality < MAX_QUALITY)
-                {
-                    item.Quality = item.Quality + 1;
+                if (item.Name != Constants.AGED_BRIE && item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.Quality > 0 && item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.Quality = item.Quality - 1;
+                if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Quality < MAX_QUALITY) item.Quality = item.Quality + 1;
+                if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 11) item.Quality = item.Quality + 1;
+                if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 6) item.Quality = item.Quality + 1;
 
-                    if (item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)
-                    {
-                        if (item.SellIn < 11)
-                            item.Quality = item.Quality + 1;
+                if (item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.SellIn = item.SellIn - 1;
+                if (item.SellIn < 0 && item.Name != Constants.AGED_BRIE && item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.Quality > 0 && item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.Quality = item.Quality - 1;
+                if (item.SellIn < 0 && item.Name != Constants.AGED_BRIE && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) item.Quality = item.Quality - item.Quality;
+                if (item.SellIn < 0 && item.Name == Constants.AGED_BRIE && item.Quality < MAX_QUALITY) item.Quality = item.Quality + 1;
 
-                        if (item.SellIn < 6)
-                            item.Quality = item.Quality + 1;
-                    }
-                }
-
-                if (item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS)
-                    item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != Constants.AGED_BRIE)
-                    {
-                        if (item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)
-                        {
-                            if (item.Quality > 0 && item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS)
-                                item.Quality = item.Quality - 1;
-                        }
-                        else
-                            item.Quality = item.Quality - item.Quality;
-                    }
-                    else if (item.Quality < MAX_QUALITY)
-                        item.Quality = item.Quality + 1;
-                }
             }
         }
     }
