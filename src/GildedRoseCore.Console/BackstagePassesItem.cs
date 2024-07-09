b7a07903dde2533004a4bfe99136192e84f80cf1
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-09 16:50:51 +0530

    Backstage Passes Update Function move to its seperate class.

---
+++ src/GildedRoseCore.Console/BackstagePassesItem.cs
@@ -0,0 +1,21 @@
+﻿namespace GildedRoseCore.Console
+{
+    public class BackstagePassesItem
+    {
+        private readonly Item item;
+
+        public BackstagePassesItem(Item item)
+        {
+            this.item = item;
+        }
+
+        public void Update()
+        {
+            if (item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Quality < 50 && item.SellIn < 11) item.Quality = item.Quality + 1;
+            if (item.Quality < 50 && item.SellIn < 6) item.Quality = item.Quality + 1;
+            if (item.SellIn <= 0) item.Quality = item.Quality - item.Quality;
+        }
+    }
+
+}
--- src/GildedRoseCore.Console/GildedRose.cs
+++ src/GildedRoseCore.Console/GildedRose.cs
@@ -27,10 +27,7 @@ namespace GildedRoseCore.Console
 
         public void BackstagePassesUpdate(Item item)
         {
-            if (item.Quality < 50) item.Quality = item.Quality + 1;
-            if (item.Quality < 50 && item.SellIn < 11) item.Quality = item.Quality + 1;
-            if (item.Quality < 50 && item.SellIn < 6) item.Quality = item.Quality + 1;
-            if (item.SellIn <= 0) item.Quality = item.Quality - item.Quality;
+            new BackstagePassesItem(item).Update();
         }
 
         public void AgedBrieUpdate(Item item)
@@ -49,4 +46,5 @@ namespace GildedRoseCore.Console
             if (item.SellIn < 0 && item.Quality > 0) item.Quality = item.Quality - 1;
         }
     }
+
 }
