74d0384bf7d5c7c9325a843f907acf5fe5d0ca4b
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-09 16:54:46 +0530

    Normal Update Function Move to its seperate class.

--- src/GildedRoseCore.Console/GildedRose.cs
+++ src/GildedRoseCore.Console/GildedRose.cs
@@ -42,9 +42,7 @@ namespace GildedRoseCore.Console
 
         public void NormalUpdate(Item item)
         {
-            if (item.Quality > 0) item.Quality = item.Quality - 1;
-            item.SellIn = item.SellIn - 1;
-            if (item.SellIn < 0 && item.Quality > 0) item.Quality = item.Quality - 1;
+            new NormalItem(item).Update();
         }
     }
 
---
+++ src/GildedRoseCore.Console/NormalItem.cs
@@ -0,0 +1,20 @@
+﻿namespace GildedRoseCore.Console
+{
+    public class NormalItem
+    {
+        private readonly Item item;
+
+        public NormalItem(Item item)
+        {
+            this.item = item;
+        }
+
+        public void Update()
+        {
+            if (item.Quality > 0) item.Quality = item.Quality - 1;
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn < 0 && item.Quality > 0) item.Quality = item.Quality - 1;
+        }
+    }
+
+}
