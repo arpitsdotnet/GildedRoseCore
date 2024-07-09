85a659b11e446e8bf4e3c679903a9b2c02fddf11
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-09 16:01:48 +0530

    Remove Unnessarry Conditions.

--- src/GildedRoseCore.Console/GildedRose.cs
+++ src/GildedRoseCore.Console/GildedRose.cs
@@ -22,12 +22,12 @@ namespace GildedRoseCore.Console
             {
                 if (item.Name != Constants.AGED_BRIE && item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.Quality > 0 && item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.Quality = item.Quality - 1;
                 if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Quality < MAX_QUALITY) item.Quality = item.Quality + 1;
-                if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 11) item.Quality = item.Quality + 1;
-                if ((item.Name == Constants.AGED_BRIE || item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 6) item.Quality = item.Quality + 1;
+                if (item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 11) item.Quality = item.Quality + 1;
+                if (item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.SellIn < 6) item.Quality = item.Quality + 1;
 
                 if (item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.SellIn = item.SellIn - 1;
                 if (item.SellIn < 0 && item.Name != Constants.AGED_BRIE && item.Name != Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT && item.Quality > 0 && item.Name != Constants.SULFURAS_HAND_OF_RAGNAROS) item.Quality = item.Quality - 1;
-                if (item.SellIn < 0 && item.Name != Constants.AGED_BRIE && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) item.Quality = item.Quality - item.Quality;
+                if (item.SellIn < 0 && item.Name == Constants.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) item.Quality = item.Quality - item.Quality;
                 if (item.SellIn < 0 && item.Name == Constants.AGED_BRIE && item.Quality < MAX_QUALITY) item.Quality = item.Quality + 1;
 
             }
